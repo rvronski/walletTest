7f9dd38cf75b658087a9100cd4b73a9276f2b6b4
rvronski <rvronski@gmail.com>
2023-01-21 20:01:29 +0500

    Initial Commit

---
+++ WalletTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WalletTest
+//
+//  Created by ROMAN VRONSKY on 21.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
